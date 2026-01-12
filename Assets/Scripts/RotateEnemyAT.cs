e1cb4529ffb24bacc562b41eac14ee660161065c
abdulaar <abdulaar@OA-S318-17>
2026-01-12 11:38:40 -0500

    normalized line.

--- Assets/Scripts/RotateEnemyAT.cs
+++ Assets/Scripts/RotateEnemyAT.cs
@@ -15,6 +15,7 @@ namespace NodeCanvas.Tasks.Actions {
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
 			return null;
+
 		}
 
 		//This is called once each time the task is enabled.
