dec58164f2ca7dd9130030a53f1a50327d6d9bcf
abdulaar <abdulaar@OA-S318-17>
2026-01-12 11:37:38 -0500

    added nodes.

---
+++ Assets/Scripts/RotateEnemyAT.cs
@@ -0,0 +1,51 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class RotateEnemyAT : ActionTask {
+
+		public GameObject enemy;
+		float rotated;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+            
+        }
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate()
+		{
+            float rotation = 90f * Time.deltaTime; 
+			enemy.transform.Rotate(0f, 0f, rotation); 
+			rotated += rotation; 
+			if (rotated >= 90f) 
+			{ 
+				EndAction(true); 
+			}
+
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
